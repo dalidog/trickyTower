a34dd7b579b6f1b5f78270d6ac8f371e92b2cc69
dalidog <league@WTS-IM-17.attlocal.net>
2017-05-21 11:34:40 -0700

    sdfghjkl

--- src/trickyTower/Block.java
+++ src/trickyTower/Block.java
@@ -2,8 +2,10 @@ package trickyTower;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class Block {
+	ArrayList<Block> ArrayBlocks = new ArrayList();
 	int X;
 	int Y;
 	int mouseX;
@@ -14,62 +16,75 @@ public class Block {
 	boolean isFalling = false;
 	int gravity = 1;
 	int Xfalling;
-	boolean moveWithPlatform=false;
-	public Block(int X, int Y, Color color){
-		this.X=X;
-		this.Y=Y;
-		this.color=color;
-	}
-void setup(){
+	int difference;
+	boolean moveWithPlatform = false;
 
-	X=mouseX;
-	Y=mouseY;
-	}
-void update(){
-
-//	if(X>platform.X && X<platform.X + 200 && Y>  platform.Y - 100 && Y< platform.Y + 200){
-//		Yvel = 0;
-//		gravity = 0;
-//	moveWithPlatform = true;
-//	}
-//	if(moveWithPlatform){
-//		X=platform.X;
-//	}
-
-}
-void draw(Graphics g){
-	if(X>platform.X && X<platform.X + 200 && Y>  platform.Y - 100 && Y< platform.Y + 200){
-		Yvel = 0;
-		gravity = 0;
-	moveWithPlatform = true;
-	}
-	if(moveWithPlatform){
-		X=platform.X;
+	public Block(int X, int Y, Color color) {
+		this.X = X;
+		this.Y = Y;
+		this.color = color;
 	}
 
-	if(isFalling){
-		//X=Xfalling;
-		Y=Y+Yvel;
-		Yvel = Yvel +gravity;
-	}
-	if(X>400){
-		X=400;
+	void setup() {
+
+		X = mouseX;
+		Y = mouseY;
 	}
 
-	g.setColor(color);
-	g.fillRect(X, Y, 100, 100);
+	void update() {
 
-}
-public boolean isFalling() {
-	return isFalling;
-}
-public void setFalling(boolean isFalling) {
-	this.isFalling = isFalling;
-}
-public int getXfalling() {
-	return Xfalling;
-}
-public void setXfalling(int xfalling) {
-	Xfalling = xfalling;
-}
+	}
+
+	void draw(Graphics g) {
+		if (X + 50 > platform.X && X < platform.X + 200 && Y > platform.Y - 100 && Y < platform.Y + 200) {
+			Yvel = 0;
+			gravity = 0;
+			moveWithPlatform = true;
+		
+			ArrayBlocks.add(new Block(X, Y, color));
+		}
+		if (moveWithPlatform) {
+
+			X = platform.X + difference;
+
+		}
+
+		if (isFalling) {
+			Y = Y + Yvel;
+			Yvel = Yvel + gravity;
+
+			if (platform.Y == Y + 89) {
+				if (X > platform.X) {
+					difference = X - platform.X;
+				} else if (X <= platform.X) {
+					difference = platform.X - X;
+				}
+
+			}
+
+		}
+		if (X > 400) {
+			X = 400;
+		}
+
+		g.setColor(color);
+		g.fillRect(X, Y, 100, 100);
+
+	}
+
+	public boolean isFalling() {
+		return isFalling;
+	}
+
+	public void setFalling(boolean isFalling) {
+		this.isFalling = isFalling;
+	}
+
+	public int getXfalling() {
+		return Xfalling;
+	}
+
+	public void setXfalling(int xfalling) {
+		Xfalling = xfalling;
+	}
 }
--- src/trickyTower/GamePanel.java
+++ src/trickyTower/GamePanel.java
@@ -15,9 +15,11 @@ public class GamePanel extends JPanel implements ActionListener, MouseMotionList
 	static Platform p = new Platform(20, 200, Color.gray);
 	Block b = new Block(50, 50, Color.BLUE);
 	Timer t = new Timer(1000 / 60, this);
-public GamePanel(){
-	t.start();
-}
+
+	public GamePanel() {
+		t.start();
+	}
+
 	protected void paintComponent(Graphics g) {
 		g.setColor(Color.pink);
 		g.fillRect(0, 0, 500, 800);
@@ -27,45 +29,57 @@ public GamePanel(){
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
+		if (b.isFalling() == false) {
+			blockcollided();
+		}
 		b.update();
 		repaint();
 	}
+
+	public void blockcollided() {
+		b = new Block(50, 50, Color.ORANGE);
+		System.out.println(" hi ");
+	}
+
 	@Override
 	public void mouseDragged(MouseEvent e) {
 
-		
 	}
+
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
-		b.X=e.getX();
+		b.X = e.getX();
 	}
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		b.setXfalling(b.X);
-		b.setFalling(true);
-		
+
 	}
+
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+		b.setXfalling(b.X);
+		b.setFalling(true);
 	}
+
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
