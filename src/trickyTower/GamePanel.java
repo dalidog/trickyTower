6853e07c1e7c956804aeecd5a6d82f7f98dfa393
Leagueathyst <league@WTS5.attlocal.net>
2017-07-02 11:30:29 -0700

    qwewrtuolp'/jfssw1245678uijjjjkkjkk

--- src/trickyTower/GamePanel.java
+++ src/trickyTower/GamePanel.java
@@ -8,41 +8,44 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, MouseMotionListener, MouseListener {
-	ArrayList<Block> ArrayBlocks = new ArrayList<Block>();
+	ArrayList<Block> arrayBlocks = new ArrayList<Block>();
 	static Platform p = new Platform(20, 200, Color.gray);
-	Block b = new Block(50, 50, 100, 100, Color.BLUE);
+
 	Timer t = new Timer(1000 / 60, this);
 	int counter = 0;
 
 	public GamePanel() {
-		ArrayBlocks.add(b);
+		arrayBlocks.add(new Block(50, 50, 100, 100, Color.BLUE));
 		t.start();
 	}
 
 	protected void paintComponent(Graphics g) {
-		g.setColor(Color.pink);
-		g.fillRect(0, 0, 500, 800);
+		drawBackground(g);
 		p.draw(g);
-		b.draw(g);
-		if (ArrayBlocks.get(counter).isFalling) {
-			counter +=1;
-			ArrayBlocks.add(new Block(50, b.mouseY, 80, 80, Color.CYAN));
-		}
-		//for (int i = 0; i < ArrayBlocks.size(); i++) {
-			ArrayBlocks.get(counter).draw(g);
-		//}
+		drawBlocks(g);
 	}
-
+	public void drawBlocks(Graphics g){
+		for (int i = 0; i < arrayBlocks.size(); i++) {
+			arrayBlocks.get(i).draw(g);
+		}
+	}
+public void drawBackground(Graphics g){
+	g.setColor(Color.pink);
+	g.fillRect(0, 0, 500, 800);
+}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
-		b.update();
-		repaint();
+for (int i = 0; i < arrayBlocks.size(); i++) {
+	arrayBlocks.get(i).update();
+}
+repaint();
+		
 	}
 
 	@Override
@@ -53,8 +56,8 @@ public class GamePanel extends JPanel implements ActionListener, MouseMotionList
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < ArrayBlocks.size(); i++) {
-			ArrayBlocks.get(i).setX(e.getX());
+		for (int i = 0; i < arrayBlocks.size(); i++) {
+			arrayBlocks.get(i).setX(e.getX());
 		}
 	}
 
@@ -67,8 +70,11 @@ public class GamePanel extends JPanel implements ActionListener, MouseMotionList
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		b.setXfalling(b.X);
-		b.setFalling(true);
+		arrayBlocks.get(0).setXfalling(arrayBlocks.get(0).X);
+		arrayBlocks.get(0).setFalling(true);
+		
+		arrayBlocks.get(1).setXfalling(arrayBlocks.get(1).X);
+		arrayBlocks.get(1).setFalling(true);
 	}
 
 	@Override
